169c9687dd6ee4daffdbbc27e02fc25284a3eddb
exploring-curiosity <curiositywake@gmail.com>
2021-07-02 17:45:52 +0530

    Remove Element 0ms

---
+++ RemoveElement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==val)k++;
+            else nums[i-k]=nums[i];
+        }
+        cout<<k;
+        return nums.size()-k;
+    }
+};
