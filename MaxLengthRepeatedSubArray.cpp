590c6dfe984a28ea61b355a3ac9e1e1c4fa70206
exploring-curiosity <curiositywake@gmail.com>
2021-07-08 20:03:34 +0530

    Maximum length of Repeated subarray 144 ms

---
+++ MaxLengthRepeatedSubArray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size(),n=nums2.size(),res=0;
+        int dp[m+1][n+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(nums1[i-1]==nums2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    res=max(res,dp[i][j]);
+                }
+            }
+        }
+        return res;
+    }
+};
